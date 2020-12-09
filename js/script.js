8de31cc442b2aa93463153b73a5731a08ff9db2c
Alex Sircu <alex.sricu@gmail.com>
2020-12-09 17:44:48 +0100

    Aggiunto autoplay e stop al click

--- js/script.js
+++ js/script.js
@@ -7,9 +7,25 @@ var app = new Vue(
         "https://raw.githubusercontent.com/anaida07/MEVN-boilerplate/master/docs/Vue.js_Logo.svg.png",
         "https://miro.medium.com/max/1000/1*N_6DIrFp3b3qv3WTDohqxg.png",
         "https://aleksei-bulgak-study.github.io/presentation/images/logo.png"
-      ]
+      ],
+      play: ""
+    },
+    created: function () {
+      this.play = setInterval(
+        () => {
+          this.increaseIndex();
+        }, 2000
+      )
     },
     methods: {
+      nextImage: function() {
+        clearInterval(this.play);
+        this.increaseIndex();
+      },
+      prevImage: function() {
+        clearInterval(this.play);
+        this.decreaseIndex();
+      },
       increaseIndex: function() {
         this.imagesIndexArray++;
         if (this.imagesIndexArray == this.images.length) {
